09f6004820307f90d993513a1f531ad4abac62af
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-06-30 00:06:52 +0200

    aggiunto campo deleted a schema mongodb

--- models/game.js
+++ models/game.js
@@ -5,7 +5,11 @@ const gameschema=new mongoose.Schema({
     genere:{type:String,enum:['Sparatutto,Avventura,Racing,Sport,MMORPG','Picchiaduro','GDR','Altro']},
     datapubblicazione:{type:Date,default:Date.now,max:Date.now},
     Sviluppatore:{type:String,required:[true,"il campo sviluppatore è richiesto"]},
-    Prezzo:{type:Number,min:0,default:0}
-})
+    Prezzo:{type:Number,min:0,default:0},
+    deleted:{ type: Boolean, default: false}
+},{strict:false})//strict false consente di aggiungere campi che non avevo definito nello schema.
+//io ho aggiunto campo deleted dopo che avevo giò tirato su il db
+//dopodichè ho usato l'updatemany di mongoose per impostare a dati già presenti in DB il nuovo valore
+
 
 module.exports=mongoose.model('Gioco',gameschema)//esportandolo posso usare questo modello per interagire con DB

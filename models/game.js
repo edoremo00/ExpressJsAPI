f27868587d1a0b96c4bb3420b091cb16c971f1e8
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-06-30 01:31:00 +0200

    aggiunti nuovi generi

--- models/game.js
+++ models/game.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const gameschema=new mongoose.Schema({
     titolo:{type:String,required:[true,"Il campo Titolo è richiesto"],unique:true},
-    genere:{type:String,enum:['Sparatutto,Avventura,Racing,Sport,MMORPG','Picchiaduro','GDR','Altro']},
+    genere:{type:String,enum:["Sparatutto","Avventura","Racing","Sport","MMORPG","Picchiaduro","GDR","FPS","TPS","Altro"]},
     datapubblicazione:{type:Date,default:Date.now,max:Date.now},
     Sviluppatore:{type:String,required:[true,"il campo sviluppatore è richiesto"]},
     Prezzo:{type:Number,min:0,default:0},

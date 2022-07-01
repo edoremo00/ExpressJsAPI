819227f46169f57020772151acda4da6d8665e3a
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-07-01 02:01:36 +0200

    modificato dto per includere info piattaforma

--- DTO/gameDTO.js
+++ DTO/gameDTO.js
@@ -4,12 +4,14 @@ class gameDto{
         genere,
         datapubblicazione,
         Sviluppatore,
+        piattaforma,
         Prezzo){
             this.titolo=titolo;
             this.id=id;
             this.genere=genere;
             this.datapubblicazione=datapubblicazione;
             this.Sviluppatore=Sviluppatore;
+            this.piattaforma=piattaforma,
             this.Prezzo=Prezzo;
         }
 

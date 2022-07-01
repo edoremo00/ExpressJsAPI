3f87b1d5ca489535914b2fae6b588c1e68cf55c2
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-07-01 02:01:59 +0200

    creato DTO per utente

---
+++ DTO/userDTO.js
@@ -0,0 +1,10 @@
+class UserDTO{
+    constructor(nome,id,datanascita,fotoprofilo,Giochi){
+        this.nome=nome;
+        this.id=id;
+        this.datanascita=datanascita;
+        this.fotoprofilo=fotoprofilo;
+        this.Giochi=Giochi
+    }
+}
+module.exports=UserDTO

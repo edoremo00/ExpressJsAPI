264a908af495abe86c5d68a600444c47f1aa149c
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-07-01 02:03:17 +0200

    aggiornato mapper per riflettere nuovi modelli

--- Mapper/mapper.js
+++ Mapper/mapper.js
@@ -1,4 +1,5 @@
 const gamedto=require('../DTO/gameDTO.js')
+const userdto=require('../DTO/userDTO.js')
 const dayjs=require('dayjs')
 
 function MapGametoGameDTO(game){
@@ -8,7 +9,28 @@ function MapGametoGameDTO(game){
     genere=game.genere,
     datapubblicazione=dayjs(game.datapubblicazione).format('DD-MM-YYYY'),
     Sviluppatore=game.Sviluppatore,
+    piattaforma=game.piattaforma,
     Prezzo=game.Prezzo
  )
 }
+
+function MapUsertoUserDto(user,withgames=false){
+   if(withgames){
+      let userdtoobj= new userdto(
+         nome=user.name,
+         id=user.id,
+         datanascita=dayjs(user.datanascita).format('DD-MM-YYYY'),
+         fotoprofilo=user?.fotoprofilo 
+      )
+      userdtoobj.Giochi=user?.Giochi.map((g)=>MapGametoGameDTO(g))
+      return userdtoobj
+   }
+   return new  userdto(
+      nome=user.name,
+      id=user._id,
+      datanascita=dayjs(user.datanascita).format('DD-MM-YYYY'),
+      fotoprofilo=user?.fotoprofilo,
+   )
+}
 module.exports.MapGametoGameDTO=MapGametoGameDTO
+module.exports.MapUsertoUserDto=MapUsertoUserDto

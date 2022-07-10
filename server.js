513ac39a9259d3f25a40bf4a0e10afa6ad71048c
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-07-10 17:01:03 +0200

    gestito errore connessione con db con promise inve che con callback

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const cors=require('cors')
 const app=express()//rappresenta il mio server
 const mongoose=require('mongoose')//per connessione a mongodb
 
-mongoose.connect(process.env.CONNECTIONSTRING)/*ritorna una promise.
+
+mongoose.connect(process.env.CONNECTIONSTRING).catch(error=>console.error(`connessione database fallita, Errore: ${connerror}`))/*ritorna una promise.
     eventi come connessione fallita li posso intercettare con il catch oppure aggiungere un listener
     a evento error come fatto qua sotto*/
 //mongoose.connection rappresenta nostra connessione a database
 //ho una serie di eventi che mi informano anche sullo stato della connessione
-mongoose.connection.on('error',(connerror)=>console.error(`connessione database fallita, Errore: ${connerror}`))
+//mongoose.connection.on('error',(connerror)=>console.error(`connessione database fallita, Errore: ${connerror}`))
 //callback che viene invocata in caso di errore connessione con DB
 
 app.get('/',(req,res)=>{//test funzionamento server

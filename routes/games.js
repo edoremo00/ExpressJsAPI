b48925b86ce5ed4e00172332444d5fcab24c2e95
Edoardo Remondini <edoardo.remondini@studenti.itsincom.it>
2022-07-10 17:02:31 +0200

    logica è stata spostata da route a controller

--- routes/games.js
+++ routes/games.js
@@ -1,91 +1,17 @@
 const express=require('express')
 const router=express.Router()
-const gamemodel=require('../models/game.js')
-const mapper=require('../Mapper/mapper.js')
 const custommiddleware=require('../custommiddlewares/custommiddlewares.js')
-
+const gamescontroller=require('../controllers/gamescontroller.js')
 //qui setterò tutte le route per fare le CRUD su DB
 
-router.get('/getallgames',async(req,res)=>{
-    let limit=req.query.limit//parametro opzionale in query string per limitare numero risultati query
-    try{
-        if(limit){
-            if(isNaN(limit)) return res.status(400).json({message:"limit non è un numero"})
-            if(limit <=0) return res.status(400).json({message:"limit deve essere maggiore di 0"})
-            const games=await gamemodel.find({deleted:false}).limit(Number(limit))
-            if(games.length>0){
-              return res.status(200).json(games.map(game=>mapper.MapGametoGameDTO(game)))
-            } 
-            return res.status(204)
-        }
-        const games=  await gamemodel.find({deleted:false})
-       return res.status(200).json(games.map(g=>mapper.MapGametoGameDTO(g)))
-    }catch(err){
-        res.status(500).json({message:err.message})
-    }
-})
+router.get('/getallgames',gamescontroller.getAllGames)
 
-router.get('/getgamebyid/:id',custommiddleware.getsingleGamemiddleware,async(req,res)=>{
-    try{
-        let gametofind=await res.game
-        if(gametofind==null||gametofind.deleted===true) return res.status(404).json({message:`Gioco con id ${req.params.id} non trovato`})
-        return res.status(200).json(mapper.MapGametoGameDTO(gametofind))
-    }
-    catch(error){
-        return res.status(500).json({message:error.message})
-    }
-})
+router.get('/getgamebyid/:id',custommiddleware.getsingleGamemiddleware,gamescontroller.getGamebyId)
 
-router.put('/updategame/:id',custommiddleware.getsingleGamemiddleware,async(req,res)=>{
-    try{
-        const gametoupdate=await res.game//risposta del middleware è in res.game
-        gametoupdate.titolo=req.body.titolo ?? gametoupdate.titolo
-        gametoupdate.genere=req.body.genere ?? gametoupdate.genere,
-        gametoupdate.datapubblicazione=req.body.datapubblicazione ??  gametoupdate.datapubblicazione,
-        gametoupdate.Sviluppatore=req.body.Sviluppatore ??  gametoupdate.Sviluppatore,
-        gametoupdate.Prezzo=req.body.Prezzo ??  gametoupdate.Prezzo,
-        gametoupdate.deleted=req.body.deleted ?? gametoupdate.deleted
+router.put('/updategame/:id',custommiddleware.getsingleGamemiddleware,gamescontroller.updateGame)
 
-        const updatedgame= await gametoupdate.save()
-        return res.status(200).json(mapper.MapGametoGameDTO(updatedgame))
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-})
+router.post('/creategame',gamescontroller.createGame)
 
-router.post('/creategame',async(req,res)=>{
-    let gioco=new gamemodel({
-        titolo:req.body.titolo,
-        genere:req.body.genere,
-        datapubblicazione:req.body.datapubblicazione,
-        Sviluppatore:req.body.Sviluppatore,
-        Prezzo:req.body.Prezzo,
-        piattaforma:req.body.piattaforma
-    })
-    //check di avere nel body almeno i campi obbligatori
-    if(Object.keys(req.body).length<3) return res.status(400).json({message:"Oggetto passato nel body non completo"})
-    try{
-        //VERIFICO CHE GIOCO NON SIA GIà IN CATALOGO
-        let check=await gamemodel.findOne({titolo:{$eq:gioco.titolo},piattaforma:{$eq:gioco.piattaforma},Sviluppatore:{$eq:gioco.Sviluppatore},deleted:false})
-        if(check) return res.status(400).json({message:"Gioco già in catalogo"})
-      const gametosave=  await gioco.save()
-      let gamemapped=mapper.MapGametoGameDTO(gametosave)
-      return res.status(201).json(gamemapped)
-    }catch(err){
-        return res.status(400).json({message:err.message})
-    }
-})
-
-router.delete('/deletegame/:id',custommiddleware.getsingleGamemiddleware,async(req,res)=>{
-    try{
-        const gametodelete=await res.game
-        if(gametodelete.deleted ===true) return res.status(404).json({message:`Gioco con id ${req.params.id} non trovato`})
-        gametodelete.deleted=true
-        await gametodelete.save()
-        return res.status(200).json()
-    }catch(error){
-        return res.send(500).json({message:error.message})
-    }
-})
+router.delete('/deletegame/:id',custommiddleware.getsingleGamemiddleware,gamescontroller.deleteGame)
 
 module.exports=router//esporto router
--- routes/users.js
+++ routes/users.js
@@ -1,98 +1,24 @@
 const express=require('express')
 const router=express.Router()
-const usersmodel=require('../models/user.js')
-const gamemodel=require('../models/game.js')
 const custommiddleware=require('../custommiddlewares/custommiddlewares.js')
-const mapper=require('../Mapper/mapper.js')
 
-router.post('/createuser',async (req,res)=>{
-    let utente=new usersmodel({
-        name:req.body.name,
-        datanascita:req.body.datanascita,
-        Giochi:req.body.giochi
+const usercontroller=require('../controllers/usercontroller.js')
 
-    })
-    try{
-        const usertosave=await utente.save()
-        return res.status(201).json(usertosave)
-    }catch(error){
-        return res.status(500).json({message:error.message})
-    }
-})
 
-router.patch('/addgamestouser/:userid',custommiddleware.Getsingleuser,async(req,res)=>{
-    try {
-        let gametoaddinfo={
-            titolo:req.body.titolo,
-            piattaforma:req.body.piattaforma
-        }
-        if(Object.keys(req.body).length<2) return res.status(400).json({message:`l'oggetto deve essere così composto {titolo:String,piattaforma:String}`})
-       
-        let usertoaddgame=await res.user
-        if(usertoaddgame.deleted) return res.status(404).json({message:`Utente con id ${req.params.userid} non trovato`})
-        let gametoadd=await gamemodel.findOne({titolo:gametoaddinfo.titolo,piattaforma:gametoaddinfo.piattaforma})
-        if(gametoadd==null) return res.status(404).json({message:"il Gioco da aggiungere non è stato trovato"})
-        if(usertoaddgame.Giochi.length>0){ //check che utente non abbia già il gioco
-            let check=usertoaddgame.Giochi.includes(gametoadd.id)
-           if(check){
-            return res.status(400).json({message:"L'utente possiede già il gioco"})
-           }
-           usertoaddgame.Giochi.push(gametoadd)
-           let updateduser=await usertoaddgame.save()
-           //NOTA: CON MONGOOSE è POSSIBILE FARE GLI UPDATE SIA TRAMITE LE QUERY COME UPDATEONE, UPDATEMANY ECC SIA MODIFICANDO L'OGGETTO
-           //RICEVUTO DA QUERY COME FIND PERCHè ESSE RESTITUISCONO DEI DOCUMENTI. QUANDO NOI CHIAMIAMO IL METODO SAVE
-           //MONGOOSE FA QUERY DI UPDATE SUL DATABASE SALVANDO I DATI ECCO PERCHè RIGA SEGUENTE è COMMENTATA IDEM RIGA 49
-           //https://mongoosejs.com/docs/documents.html#updating-using-queries
-           //const updateduser= await usersmodel.updateOne({_id:usertoaddgame._id},{$push:{Giochi:gametoadd}})
-            return res.status(200).json(mapper.MapUsertoUserDto(updateduser,true))
-          
-        }
-       //const updateduser=await usersmodel.updateOne({_id:usertoaddgame._id},{$push:{Giochi:gametoadd}})
-       usertoaddgame.Giochi.push(gametoadd)
-       let updateduser=await usertoaddgame.save()
-       return res.status(200).json(mapper.MapUsertoUserDto(updateduser,true))
-    } catch (error) {
-        return res.status(500).json({message:error.message})
-    }
-})
 
-router.get('/getsingleuserwithgames/:userid',async(req,res)=>{
-    let limit=req.query.limit
-    let user
-    try {
-        if(limit &&  !isNaN(limit) && limit>0){
-            user=await usersmodel.findOne({_id:req.params.userid,deleted:false}).populate({
-                path:'Giochi',
-                perDocumentLimit:limit
-            })
-            if(user==null) return res.status(404).json({message:`Utente con id ${req.params.userid} non trovato`})
-            return res.status(200).json(mapper.MapUsertoUserDto(user,true))
-        }
-        user=await usersmodel.findOne({_id:req.params.userid,deleted:false}).populate('Giochi')
-        if(user==null) return res.status(404).json({message:`Utente con id ${req.params.userid} non trovato`})
-        return res.status(200).json(mapper.MapUsertoUserDto(user,true))
-    } catch (error) {
-        return res.status(500).json({message:error.message})
-    }
-})
+router.post('/createuser',usercontroller.createUser)
 
-router.get('/getsingleuser/:userid',custommiddleware.Getsingleuser,async(req,res)=>{
-    let usertofind=await res.user
-    return res.status(200).json(mapper.MapUsertoUserDto(usertofind))
-})
+router.patch('/addgamestouser/:userid',custommiddleware.Getsingleuser,usercontroller.addgamesToUser)
 
-router.delete('/deleteuser/:userid',custommiddleware.Getsingleuser,async(req,res)=>{
-    let usertodelete=await res.user
-    try {
-        if(usertodelete.deleted) return res.status(404).json({message:`Utente con id ${req.params.userid} non trovato`})
-        usertodelete.deleted=true
-        await usertodelete.save()
-        return res.status(200)
-    } catch (error) {
-        return res.status(500).json({message:error.message})
-    }
-})
+router.get('/getsingleuserwithgames/:userid',usercontroller.getSingleUserwithGames)
 
+router.get('/getsingleuser/:userid',custommiddleware.Getsingleuser,usercontroller.getSingleUser)
+
+router.get('/getallusers',usercontroller.getAllusers)
+
+router.delete('/deleteuser/:userid',custommiddleware.Getsingleuser,usercontroller.deleteUser)
+
+router.post('/uploadprofilepicture/:userid',usercontroller.multerupload,custommiddleware.Getsingleuser,usercontroller.uploadProfilePicture)
 
 
 module.exports=router
